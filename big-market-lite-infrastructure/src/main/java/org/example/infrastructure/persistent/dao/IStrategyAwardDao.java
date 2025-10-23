f0166b3534c2d5cb32c8d80896323df44531be8f
JohnnyXLY <3459682602@qq.com>
2025-10-23 16:40:32 +0800

    feat: chap02 基础层持久化数据

---
+++ big-market-lite-infrastructure/src/main/java/org/example/infrastructure/persistent/dao/IStrategyAwardDao.java
@@ -0,0 +1,15 @@
+package org.example.infrastructure.persistent.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.example.infrastructure.persistent.po.StrategyAward;
+
+import java.util.List;
+
+/**
+ * 抽奖策略奖品明细配置 DAO
+ */
+
+@Mapper
+public interface IStrategyAwardDao {
+    List<StrategyAward> queryStrategyAwardList();
+}
