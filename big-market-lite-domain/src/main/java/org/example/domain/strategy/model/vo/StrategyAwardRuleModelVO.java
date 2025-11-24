7fc7eff9a1faaf298203f871b8cf00b64ff3a47b
JohnnyXLY <3459682602@qq.com>
2025-11-24 15:05:03 +0800

    feat: chap09 模板模式串联抽奖规则

--- big-market-lite-domain/src/main/java/org/example/domain/strategy/model/vo/StrategyAwardRuleModelVO.java
+++ big-market-lite-domain/src/main/java/org/example/domain/strategy/model/vo/StrategyAwardRuleModelVO.java
@@ -4,11 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.example.domain.strategy.service.rule.filter.factory.DefaultLogicFactory;
-import org.example.types.common.Constants;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 策略规则模型值对象，从数据库中查询得到
@@ -19,34 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class StrategyAwardRuleModelVO {
+
     private String ruleModels;
 
-    // 获取中置抽奖模型值列表
-    public String[] raffleCenterRuleModelList() {
-        if (null == ruleModels) {
-            return null;
-        }
-
-        List<String> ruleModelList = new ArrayList<>();
-        String[] ruleModelValues = ruleModels.split(Constants.SPLIT);
-
-        for (String ruleModelValue : ruleModelValues) {
-            if (DefaultLogicFactory.LogicModel.isCenter(ruleModelValue)) {
-                ruleModelList.add(ruleModelValue);
-            }
-        }
-        return ruleModelList.toArray(new String[0]);
-    }
-
-    // 获取后置抽奖模型值列表
-    public String[] raffleAfterRuleModelList() {
-        List<String> ruleModelList = new ArrayList<>();
-        String[] ruleModelValues = ruleModels.split(Constants.SPACE);
-        for (String ruleModelValue : ruleModelValues) {
-            if (DefaultLogicFactory.LogicModel.isAfter(ruleModelValue)) {
-                ruleModelList.add(ruleModelValue);
-            }
-        }
-        return ruleModelList.toArray(new String[0]);
-    }
 }
